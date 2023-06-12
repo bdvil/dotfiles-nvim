b4be0de4ac996c27a2ff020b9cd2c01398c1ac45
bdvllrs <bdvllrs@gmail.com>
2023-06-12 13:45:01 +0200

    Move telescope mapping to remap.lua

--- lua/bdvllrs/remap.lua
+++ lua/bdvllrs/remap.lua
@@ -22,6 +22,29 @@ vim.keymap.set("n", "<leader>Y", "\"+Y")
 vim.keymap.set("n", "<leader>d", "\"_d")
 vim.keymap.set("v", "<leader>d", "\"_d")
 
+vim.keymap.set("n", "<C-w>o", ":mksession! ~/.session.nvim<CR>:wincmd o<CR>")
+vim.keymap.set("n", "<C-w>u", ":source ~/.session.nvim<CR>")
+
+-- Telescope
+local builtin = require('telescope.builtin')
+
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set('n', '<C-e>', builtin.oldfiles, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>sh', builtin.search_history, {})
+vim.keymap.set('n', '<leader>fd', builtin.lsp_definitions, {})
+vim.keymap.set('n', '<leader>d', builtin.diagnostics, {})
+vim.keymap.set('n', '<leader>ls', builtin.lsp_document_symbols, {})
+vim.keymap.set('n', '<leader>fk', builtin.keymaps, {})
+vim.keymap.set('n', '<leader>qf', builtin.quickfix, {})
+vim.keymap.set('n', '<leader>ft', builtin.help_tags, {})
+
+vim.keymap.set('n', '<leader>gb', builtin.git_branches, {})
+vim.keymap.set('n', '<leader>gc', builtin.git_commits, {})
+vim.keymap.set('n', '<leader>gs', builtin.git_stash, {})
+
+
 -- Terminal
 vim.keymap.set("t", "<ESC>", "<C-\\><C-N>")
 vim.keymap.set("t", "<A-h>", "<C-\\><C-N><C-w>h")
