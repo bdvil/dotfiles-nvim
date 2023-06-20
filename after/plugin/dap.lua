6e0e4a26e90b210fed5f585c63d45f64cc0d5fef
bdvllrs <bdvllrs@gmail.com>
2023-06-20 15:48:09 +0200

    Do not highlight line of breakpoint.

--- after/plugin/dap.lua
+++ after/plugin/dap.lua
@@ -51,30 +51,30 @@ vim.api.nvim_set_hl(0, 'DapStopped', { ctermbg = 0, fg = '#98c379' })
 vim.fn.sign_define('DapBreakpoint', {
     text = '',
     texthl = 'DapBreakpoint',
-    linehl = 'DapBreakpoint',
+    -- linehl = 'DapBreakpoint',
     numhl = 'DapBreakpoint',
 })
 vim.fn.sign_define('DapBreakpointCondition', {
     text = '',
     texthl = 'DapBreakpoint',
-    linehl = 'DapBreakpoint',
+    -- linehl = 'DapBreakpoint',
     numhl = 'DapBreakpoint',
 })
 vim.fn.sign_define('DapBreakpointRejected', {
     text = '',
     texthl = 'DapBreakpoint',
-    linehl = 'DapBreakpoint',
+    -- linehl = 'DapBreakpoint',
     numhl = 'DapBreakpoint',
 })
 vim.fn.sign_define('DapLogPoint', {
     text = '',
     texthl = 'DapLogPoint',
-    linehl = 'DapLogPoint',
+    -- linehl = 'DapLogPoint',
     numhl = 'DapLogPoint',
 })
 vim.fn.sign_define('DapStopped', {
     text = '',
     texthl = 'DapStopped',
-    linehl = 'DapStopped',
+    -- linehl = 'DapStopped',
     numhl = 'DapStopped',
 })
