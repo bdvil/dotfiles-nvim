08990f258f5a59c1c5df2b48a4909577885e26a4
bdvllrs <bdvllrs@gmail.com>
2023-07-26 15:29:03 +0200

    Fix moving line with K in visual mode.

--- lua/bdvllrs/remap.lua
+++ lua/bdvllrs/remap.lua
@@ -4,7 +4,7 @@ vim.keymap.set({ "n", "x", "v" }, ")", "]", { remap = true })
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "J", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 vim.keymap.set("n", "J", "mzJ`z")
 
