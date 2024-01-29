5e81b642f1f5e4a0b6267eeba0d685b48c7c5a45
bdvllrs <bdvllrs@gmail.com>
2024-01-29 17:03:02 +0100

    Some formatting

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -4,22 +4,22 @@ return {
     "folke/neodev.nvim",
 
     {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v2.x',
+        "VonHeikemen/lsp-zero.nvim",
+        branch = "v2.x",
         dependencies = {
-            { 'neovim/nvim-lspconfig' },
+            { "neovim/nvim-lspconfig" },
             {
-                'williamboman/mason.nvim',
-                build = ':MasonUpdate',
+                "williamboman/mason.nvim",
+                build = ":MasonUpdate",
             },
-            { 'williamboman/mason-lspconfig.nvim' },
-            { 'hrsh7th/nvim-cmp' },
-            { 'hrsh7th/cmp-nvim-lsp' },
-            { 'L3MON4D3/LuaSnip' },
+            { "williamboman/mason-lspconfig.nvim" },
+            { "hrsh7th/nvim-cmp" },
+            { "hrsh7th/cmp-nvim-lsp" },
+            { "L3MON4D3/LuaSnip" },
         },
         config = function()
-            local lsp = require('lsp-zero').preset("recommended")
-            local mason_lspconfig = require('mason-lspconfig')
+            local lsp = require("lsp-zero").preset("recommended")
+            local mason_lspconfig = require("mason-lspconfig")
 
             lsp.on_attach(function(_, bufnr)
                 lsp.default_keymaps({ buffer = bufnr })
@@ -29,15 +29,15 @@ return {
 
             mason_lspconfig.setup({
                 ensure_installed = {
-                    'pyright',
-                    'pylsp',
-                    'lua_ls',
+                    "pyright",
+                    "pylsp",
+                    "lua_ls",
                 },
             })
 
 
-            local lspconfig = require 'lspconfig'
-            lspconfig.pyright.setup {
+            local lspconfig = require("lspconfig")
+            lspconfig.pyright.setup({
                 settings = {
                     pyright = {
                         analysis = {
@@ -45,8 +45,8 @@ return {
                         }
                     }
                 }
-            }
-            lspconfig.pylsp.setup {
+            })
+            lspconfig.pylsp.setup({
                 settings = {
                     pylsp = {
                         plugins = {
@@ -80,15 +80,15 @@ return {
                         },
                     }
                 },
-            }
-            lspconfig.lua_ls.setup {
+            })
+            lspconfig.lua_ls.setup({
                 settings = {
                     Lua = {
                         runtime = {
                             version = "LuaJIT",
                         },
                         diagnostics = {
-                            globals = { 'vim' },
+                            globals = { "vim" },
                         },
                         workspace = {
                             library = vim.api.nvim_get_runtime_file("", true),
@@ -99,7 +99,7 @@ return {
                         },
                     }
                 }
-            }
+            })
 
             local format_option = {
                 format_opts = {
@@ -107,29 +107,29 @@ return {
                     timeout_ms = 10000,
                 },
                 servers = {
-                    ['lua_ls'] = { 'lua' },
-                    ['pylsp'] = { 'python' },
+                    ["lua_ls"] = { "lua" },
+                    ["pylsp"] = { "python" },
                 }
             }
 
             lsp.format_on_save(format_option)
-            lsp.format_mapping('<C-l>', format_option)
+            lsp.format_mapping("<C-l>", format_option)
 
             lsp.set_sign_icons({
-                error = '✘',
-                warn = '▲',
-                hint = '⚑',
-                info = '»'
+                error = "✘",
+                warn = "▲",
+                hint = "⚑",
+                info = "»"
             })
 
             lsp.setup()
 
 
-            local cmp = require('cmp')
-            local compare = require('cmp.config.compare')
-            local cmp_action = require('lsp-zero').cmp_action()
-            local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-            local lspkind = require('lspkind')
+            local cmp = require("cmp")
+            local compare = require("cmp.config.compare")
+            local cmp_action = require("lsp-zero").cmp_action()
+            local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+            local lspkind = require("lspkind")
 
             lspkind.init({
                 symbol_map = {
@@ -140,7 +140,7 @@ return {
             vim.api.nvim_set_hl(0, "CmpItemKindCopilot", { fg = "#6CC644" })
 
             cmp.event:on(
-                'confirm_done',
+                "confirm_done",
                 cmp_autopairs.on_confirm_done()
             )
 
@@ -152,33 +152,33 @@ return {
 
             cmp.setup({
                 sources = {
-                    { name = 'path' },
-                    { name = 'nvim_lsp' },
-                    { name = 'buffer',  keyword_length = 3 },
-                    { name = 'luasnip', keyword_length = 2 },
-                    -- { name = 'copilot' },
+                    { name = "path" },
+                    { name = "nvim_lsp" },
+                    { name = "buffer",  keyword_length = 3 },
+                    { name = "luasnip", keyword_length = 2 },
+                    -- { name = "copilot" },
                 },
                 mapping = {
-                    ['<CR>'] = cmp.mapping.confirm(),
-                    ['<C-A>'] = cmp.mapping.complete(),
-                    ['<Tab>'] = vim.schedule_wrap(function(fallback)
+                    ["<CR>"] = cmp.mapping.confirm(),
+                    ["<C-A>"] = cmp.mapping.complete(),
+                    ["<Tab>"] = vim.schedule_wrap(function(fallback)
                         if cmp.visible() and has_words_before() then
                             cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
                         else
                             fallback()
                         end
                     end),
-                    ['<S-Tab>'] = cmp_action.select_prev_or_fallback(),
-                    ['<Down>'] = cmp.mapping(cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
-                        { 'i' }),
-                    ['<Up>'] = cmp.mapping(cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
-                        { 'i' }),
-                    ['<C-j>'] = cmp.mapping({
+                    ["<S-Tab>"] = cmp_action.select_prev_or_fallback(),
+                    ["<Down>"] = cmp.mapping(cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+                        { "i" }),
+                    ["<Up>"] = cmp.mapping(cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
+                        { "i" }),
+                    ["<C-j>"] = cmp.mapping({
                         c = function()
                             if cmp.visible() then
                                 cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
                             else
-                                vim.api.nvim_feedkeys(t('<Down>'), 'n', true)
+                                vim.api.nvim_feedkeys(t("<Down>"), "n", true)
                             end
                         end,
                         i = function(fallback)
@@ -189,12 +189,12 @@ return {
                             end
                         end
                     }),
-                    ['<C-k>'] = cmp.mapping({
+                    ["<C-k>"] = cmp.mapping({
                         c = function()
                             if cmp.visible() then
                                 cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select })
                             else
-                                vim.api.nvim_feedkeys(t('<Up>'), 'n', true)
+                                vim.api.nvim_feedkeys(t("<Up>"), "n", true)
                             end
                         end,
                         i = function(fallback)
@@ -205,18 +205,18 @@ return {
                             end
                         end
                     }),
-                    ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
-                    ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+                    ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
+                    ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
                 },
-                preselect = 'item',
+                preselect = "item",
                 completion = {
-                    completeopt = 'menu,menuone,noinsert',
+                    completeopt = "menu,menuone,noinsert",
                     side_padding = 5,
                 },
                 formatting = {
                     format = function(entry, vim_item)
-                        if vim.tbl_contains({ 'path' }, entry.source.name) then
-                            local icon, hl_group = require('nvim-web-devicons').get_icon(entry:get_completion_item()
+                        if vim.tbl_contains({ "path" }, entry.source.name) then
+                            local icon, hl_group = require("nvim-web-devicons").get_icon(entry:get_completion_item()
                                 .label)
                             if icon then
                                 vim_item.kind = icon
