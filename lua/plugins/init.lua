51258a75913d6d9f2acfc021cac6d8d15f90fdbe
bdvllrs <bdvllrs@gmail.com>
2024-03-04 09:36:22 +0000

    FeMaco config: float windows relative to win instead of editor.

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -52,7 +52,7 @@ return {
 			require("femaco").setup({
 				float_opts = function(_)
 					return {
-						relative = "editor",
+						relative = "win",
 						width = vim.api.nvim_win_get_width(0),
 						height = vim.api.nvim_win_get_height(0),
 						anchor = "NW",
