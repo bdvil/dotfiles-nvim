6e347b59d55c6675b9f19e25e8bf34a038557989
bdvil <benjam.devillers@gmail.com>
2024-03-19 22:45:35 +0100

    Add command to attach pyrefactorlsp

---
+++ lua/bdvllrs/pyrefactorlsp.lua
@@ -0,0 +1,12 @@
+LaunchPyrefactorLSP = function()
+    local client_id = vim.lsp.start({
+        name = "pyrefactorlsp",
+        cmd = vim.lsp.rpc.connect("127.0.0.1", 8989),
+        root_dir = vim.fs.dirname(vim.fs.find({ 'pyproject.toml', 'setup.py' }, { upward = true })[1]),
+    })
+    vim.lsp.buf_attach_client(0, client_id)
+end
+
+vim.cmd([[
+  command! -range LaunchPyrefactorLSP  execute 'lua LaunchPyrefactorLSP()'
+]])
