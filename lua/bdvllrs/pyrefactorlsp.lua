3719ff433d60c7308ec90d806c9f4cfce869e3d0
bdvil <benjam.devillers@gmail.com>
2024-04-17 21:26:38 +0200

    Add StopLSP

--- lua/bdvllrs/pyrefactorlsp.lua
+++ lua/bdvllrs/pyrefactorlsp.lua
@@ -1,5 +1,8 @@
-LaunchPyrefactorLSP = function()
-    local client_id = vim.lsp.start({
+local client_id = nil
+
+
+StartPyrefactorLSP = function()
+    client_id = vim.lsp.start({
         name = "pyrefactorlsp",
         cmd = vim.lsp.rpc.connect("127.0.0.1", 8989),
         root_dir = vim.fs.dirname(vim.fs.find({ "pyproject.toml", "setup.py" }, { upward = true })[1]),
@@ -7,6 +10,17 @@ LaunchPyrefactorLSP = function()
     vim.lsp.buf_attach_client(0, client_id)
 end
 
+StopPyrefactorLSP = function()
+    if client_id then
+        vim.lsp.buf_detach_client(0, client_id)
+        vim.lsp.stop_client(client_id)
+        client_id = nil
+    end
+end
+
 vim.cmd([[
-  command! -range LaunchPyrefactorLSP  execute 'lua LaunchPyrefactorLSP()'
+  command! -range StartPyrefactorLSP  execute 'lua StartPyrefactorLSP()'
+]])
+vim.cmd([[
+  command! -range StopPyrefactorLSP  execute 'lua StopPyrefactorLSP()'
 ]])
