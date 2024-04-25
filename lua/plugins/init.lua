35c0fb97e2578543fdff3b6f3ca3b0cf4bdc1fa0
bdvllrs <bdvllrs@gmail.com>
2024-04-25 13:52:49 +0000

    Update flash keymaps

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -13,7 +13,20 @@ return {
     "tpope/vim-repeat",
     "tpope/vim-surround",
     "tpope/vim-unimpaired",
-    "folke/flash.nvim",
+    {
+        "folke/flash.nvim",
+        event = "VeryLazy",
+        ---@type Flash.Config
+        opts = {},
+        -- stylua: ignore
+        keys = {
+            { "<leader>s", mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
+            { "<leader>S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
+            { "r",         mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
+            { "R",         mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+            { "<c-s>",     mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
+        },
+    },
 
     "mrjones2014/nvim-ts-rainbow",
 
