39a15bd506fd0a5414d836b09acae8f483e41c51
bdvil <benjam.devillers@gmail.com>
2024-04-27 15:20:45 +0200

    Better pyrefactor commands

--- lua/bdvllrs/pyrefactorlsp.lua
+++ lua/bdvllrs/pyrefactorlsp.lua
@@ -1,26 +1,59 @@
 local client_id = nil
+local n_clients = 0
+local attached_clients = {}
 
 
-StartPyrefactorLSP = function()
-    client_id = vim.lsp.start({
-        name = "pyrefactorlsp",
-        cmd = vim.lsp.rpc.connect("127.0.0.1", 8989),
-        root_dir = vim.fs.dirname(vim.fs.find({ "pyproject.toml", "setup.py" }, { upward = true })[1]),
-    })
-    vim.lsp.buf_attach_client(0, client_id)
+EnablePyrefactorLSP = function()
+    if client_id == nil then
+        client_id = vim.lsp.start({
+            name = "pyrefactorlsp",
+            cmd = vim.lsp.rpc.connect("127.0.0.1", 8989),
+            root_dir = vim.fs.dirname(vim.fs.find({ "pyproject.toml", "setup.py" }, { upward = true })[1]),
+        })
+    end
+    local buf = vim.api.nvim_get_current_buf()
+    vim.lsp.buf_attach_client(buf, client_id)
+    table.insert(attached_clients, buf)
+    n_clients = n_clients + 1
+end
+
+DisablePyrefactorLSP = function()
+    if client_id then
+        local buf = vim.api.nvim_get_current_buf()
+        vim.lsp.buf_detach_client(buf, client_id)
+        for k, v in ipairs(attached_clients) do
+            if v == buf then
+                table.remove(attached_clients, k)
+                n_clients = n_clients - 1
+                break
+            end
+        end
+    end
+    if n_clients == 0 then
+        vim.lsp.stop_client(client_id)
+        client_id = nil
+        attached_clients = {}
+    end
 end
 
 StopPyrefactorLSP = function()
     if client_id then
-        vim.lsp.buf_detach_client(0, client_id)
+        for _, buf in ipairs(attached_clients) do
+            vim.lsp.buf_detach_client(buf, client_id)
+        end
         vim.lsp.stop_client(client_id)
         client_id = nil
+        n_clients = 0
+        attached_clients = {}
     end
 end
 
 vim.cmd([[
-  command! -range StartPyrefactorLSP  execute 'lua StartPyrefactorLSP()'
+  command! -range PyreEnable  execute 'lua EnablePyrefactorLSP()'
 ]])
 vim.cmd([[
-  command! -range StopPyrefactorLSP  execute 'lua StopPyrefactorLSP()'
+  command! -range PyreDisable  execute 'lua DisablePyrefactorLSP()'
+]])
+vim.cmd([[
+  command! -range PyreStop  execute 'lua StopPyrefactorLSP()'
 ]])
