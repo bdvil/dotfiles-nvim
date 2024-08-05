c258e7c92af6dff834d35828186df65fcba6a28f
bdvllrs <bdvllrs@gmail.com>
2024-08-05 09:49:59 +0000

    change term shortcut

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -3,7 +3,7 @@ return {
         "akinsho/toggleterm.nvim",
         version = "*",
         opts = { --[[ things you want to change go here]]
-            open_mapping = [[<C-o>]],
+            open_mapping = [[<C-m>]],
         },
     },
 }
