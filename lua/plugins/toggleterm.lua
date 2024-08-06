76944adc7b5ac4ab42e4ba9d69126e4200ab8590
bdvllrs <bdvllrs@gmail.com>
2024-08-06 09:44:22 +0000

    remove toggleterm

--- lua/plugins/toggleterm.lua
+++
@@ -1,9 +0,0 @@
-return {
-    {
-        "akinsho/toggleterm.nvim",
-        version = "*",
-        opts = { --[[ things you want to change go here]]
-            open_mapping = [[<C-m>]],
-        },
-    },
-}
